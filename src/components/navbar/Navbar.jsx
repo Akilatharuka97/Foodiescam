f035ec12bcb57b9cf19d2bc075e3a6750d8c8001
Akila Tharuka <akilatharuka555@gmail.com>
2024-02-01 17:35:06 +0530

    navbar created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,42 @@
 import "./navbar.scss"
 
+import DefaultProfilePic from '../../assets/profile/default_user_icon.png'
+
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
+import Brightness7RoundedIcon from '@mui/icons-material/Brightness7Rounded';
+import GridViewRoundedIcon from '@mui/icons-material/GridViewRounded';
+import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
+import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
-        <div className="navbar">Navbar</div>
+        <div className="navbar">
+            <div className="left">
+                <Link to={"/"} style={{textDecoration: "none"}}>
+                    <span>Foodiescam</span>
+                </Link>
+                <HomeRoundedIcon/>
+                <DarkModeRoundedIcon/>
+                <GridViewRoundedIcon/>
+                <div className="search">
+                    <SearchRoundedIcon/>
+                    <input type="text" placeholder="Search Foodiescam" />
+                </div>
+            </div>
+            <div className="right">
+                <PersonRoundedIcon/>
+                <EmailRoundedIcon/>
+                <NotificationsRoundedIcon/>
+                <div className="user">
+                    <img src={DefaultProfilePic} alt="Profile Pic" />
+                    <span>Akila Tharuka</span>
+                </div>
+            </div>
+        </div>
     )
 }
 
